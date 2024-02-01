c1f484dbce335388aae5af75a22a46a8486a504b
root <root@localhost.localdomain>
2024-02-01 21:34:54 +0000

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 
-	if (leaves)
+	if (tree)
 	{
 		leaves += (!tree->left && !tree->right) ? 1 : 0;
 		leaves += binary_tree_leaves(tree->left);
